f0dfe299258f387fccfc94cbfa0fe4322e795444
max <max.schlienz@uni-konstanz.de>
2024-12-03 23:31:24 +0100

    qs slow

--- main.go
+++ main.go
@@ -9,55 +9,57 @@ package main
 import (
 	"fmt"
 	"math"
-	"runtime"
 	"sync"
 	"time"
 )
 
 func main() {
 
-	for i := 3; i < 10; i++ {
-		slice := createSlice(i)
-
-		start := time.Now()
-		qs(0, len(slice), slice)
-		duration := time.Since(start)
-		fmt.Print("Slice of size ", math.Pow10(i), " was sorted in ", duration, " using QuickSort ")
-
-		scramble(slice)
-
-		start = time.Now()
+	for i := 9; i < 10; i++ {
+		slice := createSlice(9)
+		aux := make([]int32, len(slice))
+		//start := time.Now()
+		duration := callParMS(8, slice, aux, 0)
+		fmt.Print("Slice of size ", math.Pow10(i), " was sorted in ", duration, " ms using QuickSort ")
+		/**
+		 		start = time.Now()
 		ms(slice)
 		duration = time.Since(start)
 		fmt.Print("Slice of size ", math.Pow10(i), " was sorted in ", duration, " using MergeSort ")
 
 		fmt.Println()
-	}
-
-	for i := 1; i <= runtime.NumCPU(); i++ {
-		var duration int64 = 0
-		slice := createSlice(6)
-		aux := make([]int32, len(slice))
-		copy(aux, slice)
-
-		for j := 0; j < 100; j++ {
-			duration = callParQS(i, slice, aux, duration)
-			scramble(slice)
-			copy(aux, slice)
-		}
-		fmt.Println("PARALLEL QUICKSORT With ", i, " Core(s): \n", duration, " ms total\n", duration/100, " ms average \n")
-
-		for j := 0; j < 100; j++ {
-			duration = callParMS(i, slice, aux, duration)
-			scramble(slice)
-			copy(aux, slice)
-		}
-		fmt.Println("PARALLEL MERGESORT With ", i, " Core(s): \n", duration, " ms total\n", duration/100, " ms average \n")
-		duration = 0
+		 scramble(slice)
+		*/
 
 	}
+	/**
+	  for i := 1; i <= runtime.NumCPU(); i++ {
+	  	var duration int64 = 0
+	  	slice := createSlice(6)
+	  	aux := make([]int32, len(slice))
+	  	copy(aux, slice)
+
+	  	for j := 0; j < 100; j++ {
+	  		duration = callParQS(i, slice, aux, duration)
+	  		scramble(slice)
+	  		copy(aux, slice)
+	  	}
+	  	fmt.Println("PARALLEL QUICKSORT With ", i, " Core(s): \n", duration, " ms total\n", duration/100, " ms average \n")
+
+	  	for j := 0; j < 100; j++ {
+	  		duration = callParMS(i, slice, aux, duration)
+	  		scramble(slice)
+	  		copy(aux, slice)
+	  	}
+	  	fmt.Println("PARALLEL MERGESORT With ", i, " Core(s): \n", duration, " ms total\n", duration/100, " ms average \n")
+	  	duration = 0
+
+	  }
+	*/
+
 }
 
+/**
 func callParQS(i int, slice []int32, aux []int32, duration int64) int64 {
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -68,6 +70,7 @@ func callParQS(i int, slice []int32, aux []int32, duration int64) int64 {
 	duration += time.Since(start).Milliseconds()
 	return duration
 }
+*/
 
 func callParMS(i int, slice []int32, aux []int32, duration int64) int64 {
 	semaphore := make(chan struct{}, i)
